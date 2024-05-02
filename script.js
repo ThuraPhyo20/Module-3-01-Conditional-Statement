538350ed36d4d921f93535a9cdbfa03c82671744
ThuRaPhyo <thuyaphyo7877@gmail.com>
2024-05-02 13:19:29 +0630

    solved

---
+++ script.js
@@ -0,0 +1,16 @@
+// TODO: Change the values and operators below to test your algorithm meets all conditions
+var x = 50;
+var exp1 = x < 25;
+var exp2 = x > 50;
+
+// TODO: Write Your JavaScript Code Here
+
+if (exp1 && exp2) {
+    console.log('True True');
+} else if (exp1) {
+    console.log('True False');
+} else if (exp2) {
+    console.log('False True');
+} else {
+    console.log('False False')
+}
